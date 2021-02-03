f5e8a2a08a02706f6a2906b77ba14ab361dd4c8e
Chris Keller <christopherlawrencekeller@gmail.com>
2021-02-02 21:47:25 -0700

    updates dashboard script to make data the right way

--- compile-daily-dashboard-data.py
+++ compile-daily-dashboard-data.py
@@ -2,16 +2,13 @@
 # -*- coding: utf-8 -*-
 
 from bs4 import BeautifulSoup
-import csv
 import json
 import datetime
 from itertools import zip_longest
-from collections import OrderedDict
 import os
 from os import listdir
 from os.path import isfile, join
 import logging
-import pandas as pd
 
 logger = logging.getLogger("root")
 logging.basicConfig(
@@ -19,129 +16,121 @@ logging.basicConfig(
     level=logging.DEBUG
 )
 
-timestamp = datetime.datetime.now().strftime("%Y-%m-%d-%H%M%S")
 
-dir_current = os.path.dirname(os.path.realpath(__file__))
+class ProcessHtml(object):
 
-dir_html = "daily-html"
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d-%H%M%S")
 
-dir_data = "daily-data"
+    dir_current = os.path.dirname(os.path.realpath(__file__))
 
-file_path = os.path.join(dir_current, dir_html)
+    dir_html = "daily-html"
 
-files = [f for f in listdir(file_path) if isfile(join(file_path, f))]
+    dir_data = "daily-data"
 
-output = []
+    path = os.path.join(dir_current, dir_html)
 
-for file in files:
+    output = []
 
-    compiled_data = []
+    def handle(self):
+        files = [f for f in listdir(self.path) if isfile(join(self.path, f))]
+        self.process(files)
+        json_f = {
+            "file": "latest-nm-vaccine-dashboard.json"
+        }
+        self.write_json_file(json_f, self.output)
 
-    target = os.path.join(file_path, file)
-    with open(target, 'r', encoding='utf-8') as f:
-        soup = BeautifulSoup(f, 'lxml')
+    def process(self, files):
+        for file in files:
+            compiled_data = []
+            target = os.path.join(self.path, file)
+            with open(target, 'r', encoding='utf-8') as f:
+                soup = self.get_soup(f)
+                acquired_datestamp = file[:17]
+                last_updated = soup.find_all("i")[0].get_text().strip().replace("Last Updated: ", "")
+                topline = self.get_topline_figures(soup, acquired_datestamp, last_updated)
+                compiled_data.append(topline)
+                subgroup = self.get_subgroup(soup, last_updated)
+                compiled_data.extend(subgroup)
+            self.output.extend(compiled_data)
+            json_f = {
+                "file": "{0}-nm-vaccine-dashboard.json".format(file[:17])
+            }
+            self.write_json_file(json_f, compiled_data)
 
-        last_updated = soup.find_all("i")[0].get_text().strip()
-        last_updated = last_updated.replace("Last Updated: ", "")
+    def write_json_file(self, file, data):
+        file_saved = os.path.join(
+            self.dir_current,
+            self.dir_data,
+            file['file']
+        )
+        with open(file_saved, 'w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+            logger.debug('File saved to {0}'.format(file_saved))
 
-        # get the topline numbers
+    def get_subgroup(self, soup, last_updated):
+        """Get the manufacturer, age, gender, race and ethnicity figures."""
+        output = []
+        tables = soup.find_all(
+            "table", attrs={"class": "data-table"}
+        )
+        for table in tables[1:]:
+            table_dict = {
+                "type": None,
+                "date": last_updated,
+                "data": []
+            }
+            keys = [i.get_text().strip() for i in table.find_all('th')]
+            rows = table.find_all('tr')
+            for row in rows[1:]:
+                items = [i.get_text().strip() for i in row.find_all('td')]
+                item = dict(zip(keys, items))
+                if "Vaccine Manufacturer" in item:
+                    table_dict['type'] = 'manufacturer'
+                    table_dict['data'].append(item)
+                elif "Age Group" in item:
+                    table_dict['type'] = 'age_groups'
+                    table_dict['data'].append(item)
+                elif "Gender" in item:
+                    table_dict['type'] = 'gender'
+                    table_dict['data'].append(item)
+                elif "Race" in item:
+                    table_dict['type'] = 'race'
+                    table_dict['data'].append(item)
+                elif "Ethnicity" in item:
+                    table_dict['type'] = 'ethnicity'
+                    table_dict['data'].append(item)
+                else:
+                    logger.debug("Something went really sideways")
+            output.append(table_dict)
+        return output
+
+    def get_topline_figures(self, soup, acquired_datestamp, last_updated):
+        """Get the topline numbers."""
         topline_keys = []
         keys = soup.find_all(
-            "div", attrs={"class" : "graph-sub-num-title"}
+            "div", attrs={"class": "graph-sub-num-title"}
         )
         for key in keys:
             topline_keys.append(key.get_text().strip())
-
         topline_values = []
         values = soup.find_all(
-            "div", attrs={"class" : "big-number"}
+            "div", attrs={"class": "big-number"}
         )
         for value in values:
             topline_values.append(value.get_text().strip())
-
-        compiled_topline = dict(zip_longest(topline_keys, topline_values))
-        compiled_topline['acquired_datestamp'] = file[:17]
-        toplineDict = {
+        topline_dict = {
             "type": "topline",
             "date": last_updated,
-            "data": compiled_topline
+            "data": dict(zip_longest(topline_keys, topline_values))
         }
-        compiled_data.append(toplineDict)
+        topline_dict["data"]["datestamp"] = acquired_datestamp
+        return topline_dict
 
-        # get the manufacturer, age, gender, race and ethnicity figures
-        subgroup_output = []
-        subgroup_data = []
-        tables = soup.find_all(
-            "table", attrs={"class" : "data-table"}
-        )
-        for table in tables[1:]:
-            keys =[i.get_text().strip() for i in table.find_all('th')]
-            rows = table.find_all('tr')
-            vals = []
-            for row in rows[1:]:
-                items = [i.get_text().strip() for i in row.find_all('td')]
-                my_dict = dict(zip(keys, items))
-                subgroup_data.append(my_dict)
-
-        newDict = {
-            "type": None,
-            "date": last_updated,
-            "data": []
-        }
-
-        for item in subgroup_data:
-
-            if "Vaccine Manufacturer" in item:
-                logger.debug(item)
-                newDict['type'] = 'manufacturer'
-                newDict['data'].append(item)
-
-            # elif "Age Group" in item:
-            #     newDict['type'] = 'age_groups'
-            #     newDict['data'].append(item)
-            # elif "Gender" in item:
-            #     newDict['type'] = 'gender'
-            #     newDict['data'].append(item)
-            # elif "Race" in item:
-            #     newDict['type'] = 'race'
-            #     newDict['data'].append(item)
-            # elif "Ethnicity" in item:
-            #     newDict['type'] = 'ethnicity'
-            #     newDict['data'].append(item)
-            # else:
-            #     logger.debug(item)
-
-        subgroup_output.append(newDict)
+    def get_soup(self, file):
+        soup = BeautifulSoup(file, 'lxml')
+        return soup
 
 
-        logger.debug(subgroup_output)
-
-
-
-
-
-    # compile everything from one file into one big list
-    # compiled_data.extend(subgroup_data)
-    # logger.debug(compiled_data)
-
-    # output_json = [
-    #     # {"file": "latest-nm-vaccine-dashboard.json"},
-    #     {"file": "{0}-nm-vaccine-dashboard.json".format(file[:17])},
-    # ]
-
-    # output_csv = [
-    #     # {"file": "latest-nm-vaccine-dashboard.csv"},
-    #     {"file": "{0}-nm-vaccine-dashboard.csv".format(file[:17])},
-    # ]
-
-    # for f in output_json:
-    #     file_saved = os.path.join(dir_current, dir_data, f['file'])
-    #     with open(file_saved, 'w', encoding='utf-8') as f:
-    #         json.dump(compiled_data, f, ensure_ascii=False, indent=4)
-    #         logger.debug('File saved to {0}'.format(file_saved))
-
-# for f in output_csv:
-#     file_saved = os.path.join(dir_current, dir_data, f['file'])
-#     csv_data = pd.DataFrame(output)
-#     csv_data.to_csv(file_saved, encoding='utf-8', index=False)
-#     logger.debug('File saved to {0}'.format(file_saved))
+if __name__ == '__main__':
+    task_run = ProcessHtml()
+    task_run.handle()
