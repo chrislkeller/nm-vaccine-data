c012601a7012f4213dcbaf0bf984b2397f925d71
Chris Keller <christopherlawrencekeller@gmail.com>
2021-02-04 23:34:53 -0700

    updates the compilation of vaccine dashboard data

--- compile-daily-dashboard-data.py
+++ compile-daily-dashboard-data.py
@@ -3,11 +3,13 @@
 
 from bs4 import BeautifulSoup
 import json
+import csv
 import datetime
 from itertools import zip_longest
 import os
 from os import listdir
 from os.path import isfile, join
+import locale
 import logging
 
 logger = logging.getLogger("root")
@@ -16,121 +18,178 @@ logging.basicConfig(
     level=logging.DEBUG
 )
 
+locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
 
-class ProcessHtml(object):
-
+class CompileJsonFiles(object):
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d-%H%M%S")
-
     dir_current = os.path.dirname(os.path.realpath(__file__))
-
     dir_html = "daily-html"
-
     dir_data = "daily-data"
-
     path = os.path.join(dir_current, dir_html)
 
-    output = []
-
     def handle(self):
+        latest_output = []
         files = [f for f in listdir(self.path) if isfile(join(self.path, f))]
-        self.process(files)
-        json_f = {
-            "file": "latest-nm-vaccine-dashboard.json"
-        }
-        self.write_json_file(json_f, self.output)
-
-    def process(self, files):
         for file in files:
-            compiled_data = []
+            daily_output = []
             target = os.path.join(self.path, file)
             with open(target, 'r', encoding='utf-8') as f:
                 soup = self.get_soup(f)
-                acquired_datestamp = file[:17]
-                last_updated = soup.find_all("i")[0].get_text().strip().replace("Last Updated: ", "")
-                topline = self.get_topline_figures(soup, acquired_datestamp, last_updated)
-                compiled_data.append(topline)
-                subgroup = self.get_subgroup(soup, last_updated)
-                compiled_data.extend(subgroup)
-            self.output.extend(compiled_data)
-            json_f = {
-                "file": "{0}-nm-vaccine-dashboard.json".format(file[:17])
-            }
-            self.write_json_file(json_f, compiled_data)
+                processed_data = self.process(file, soup)
+                daily_output.append(processed_data)
+                latest_output.append(processed_data)
+            self.write_json_file("{0}-nm-vaccine-dashboard.json".format(file[:17]), daily_output)
+        self.write_json_file("latest-nm-vaccine-dashboard.json", daily_output)
 
     def write_json_file(self, file, data):
         file_saved = os.path.join(
             self.dir_current,
             self.dir_data,
-            file['file']
+            file
         )
         with open(file_saved, 'w', encoding='utf-8') as f:
             json.dump(data, f, ensure_ascii=False, indent=4)
             logger.debug('File saved to {0}'.format(file_saved))
 
-    def get_subgroup(self, soup, last_updated):
+    def process(self, file, soup):
+        html_date = soup.find_all("i")[0].get_text().strip()
+        last_updated = html_date.replace("Last Updated: ", "")
+        topline = self.get_topline_figures(soup, last_updated, file)
+        subgroups = self.get_subgroup_figures(soup, last_updated, file)
+        return {
+            "topline": topline,
+            "subgroups": subgroups
+        }
+
+    def get_topline_figures(self, soup, last_updated, file):
+        """Get the topline numbers."""
+        output = {
+            "type": None,
+            "date": last_updated,
+            "acquired_datestamp": file[:17],
+            "data": []
+        }
+        element = soup.find_all(
+            "div", attrs={"class": "element"}
+        )
+        compiled = {}
+        for e in element:
+            key = e.find(
+                "div", attrs={"class": "graph-sub-num-title"}
+            ).get_text().strip()
+            value = e.find(
+                "div", attrs={"class": "big-number"}
+            ).get_text().strip()
+            compiled[key] = value
+            compiled["acquired_datestamp"] = output['acquired_datestamp']
+        output["data"].append(compiled)
+        output["type"] = "topline"
+        return output
+
+    def get_subgroup_figures(self, soup, last_updated, file):
         """Get the manufacturer, age, gender, race and ethnicity figures."""
-        output = []
+        output = {
+            "type": None,
+            "date": last_updated,
+            "acquired_datestamp": file[:17],
+            "data": []
+        }
         tables = soup.find_all(
             "table", attrs={"class": "data-table"}
         )
         for table in tables[1:]:
-            table_dict = {
-                "type": None,
-                "date": last_updated,
-                "data": []
-            }
             keys = [i.get_text().strip() for i in table.find_all('th')]
             rows = table.find_all('tr')
             for row in rows[1:]:
                 items = [i.get_text().strip() for i in row.find_all('td')]
-                item = dict(zip(keys, items))
-                if "Vaccine Manufacturer" in item:
-                    table_dict['type'] = 'manufacturer'
-                    table_dict['data'].append(item)
-                elif "Age Group" in item:
-                    table_dict['type'] = 'age_groups'
-                    table_dict['data'].append(item)
-                elif "Gender" in item:
-                    table_dict['type'] = 'gender'
-                    table_dict['data'].append(item)
-                elif "Race" in item:
-                    table_dict['type'] = 'race'
-                    table_dict['data'].append(item)
-                elif "Ethnicity" in item:
-                    table_dict['type'] = 'ethnicity'
-                    table_dict['data'].append(item)
+                """
+                modify list entries during for loop
+                """
+                for i, s in enumerate(items):
+                    new_value = self._to_num(s)
+                    if new_value:
+                        items[i] = new_value['value']
+                    else:
+                        pass
+                compiled = dict(zip(keys, items))
+                compiled["acquired_datestamp"] = output['acquired_datestamp']
+                if "Vaccine Manufacturer" in compiled:
+                    output['type'] = 'manufacturer'
+                    output['data'].append(compiled)
+                elif "Age Group" in compiled:
+                    output['type'] = 'age_groups'
+                    output['data'].append(compiled)
+                elif "Gender" in compiled:
+                    output['type'] = 'gender'
+                    output['data'].append(compiled)
+                elif "Race" in compiled:
+                    output['type'] = 'race'
+                    output['data'].append(compiled)
+                elif "Ethnicity" in compiled:
+                    output['type'] = 'ethnicity'
+                    output['data'].append(compiled)
                 else:
                     logger.debug("Something went really sideways")
-            output.append(table_dict)
+            output["data"].append(compiled)
         return output
-
-    def get_topline_figures(self, soup, acquired_datestamp, last_updated):
-        """Get the topline numbers."""
-        topline_keys = []
-        keys = soup.find_all(
-            "div", attrs={"class": "graph-sub-num-title"}
-        )
-        for key in keys:
-            topline_keys.append(key.get_text().strip())
-        topline_values = []
-        values = soup.find_all(
-            "div", attrs={"class": "big-number"}
-        )
-        for value in values:
-            topline_values.append(value.get_text().strip())
-        topline_dict = {
-            "type": "topline",
-            "date": last_updated,
-            "data": dict(zip_longest(topline_keys, topline_values))
-        }
-        topline_dict["data"]["datestamp"] = acquired_datestamp
-        return topline_dict
 
     def get_soup(self, file):
         soup = BeautifulSoup(file, 'lxml')
         return soup
 
+    def _to_num(self, value):
+        """
+        given a value can it be converted to an int
+        http://stackoverflow.com/a/16464365
+        """
+        output = {}
+        NoneType = type(None)
+        # actually integer values
+        if isinstance(value, (int)):
+            output["convert"] = True
+            output["value"] = value
+            output["type"] = type(value)
+        # some floats can be converted without loss
+        elif isinstance(value, float):
+            output["convert"] = (int(value) == float(value))
+            output["value"] = value
+            output["type"] = type(value)
+        # we can't convert nonetypes
+        elif isinstance(value, NoneType):
+            output["convert"] = False
+            output["value"] = None
+            output["type"] = type(value)
+        # we can't convert non-string
+        elif not isinstance(value, str):
+            output["convert"] = False
+            output["value"] = "Nonstring"
+            output["type"] = type(value)
+        else:
+            value = value.strip()
+            try:
+                # try to convert value to float
+                float_value = float(value)
+                output["convert"] = True
+                output["value"] = float_value
+                output["type"] = type(float_value)
+            except ValueError:
+                # if fails try to convert value to int
+                try:
+                    # int_value = int(value)
+                    int_value = locale.atoi(value)
+                    output["convert"] = True
+                    output["value"] = int_value
+                    output["type"] = type(int_value)
+                # if fails it's a string
+                except ValueError:
+                    output["convert"] = False
+                    output["value"] = None
+                    output["type"] = type(value)
+        if output["convert"] != False:
+            return output
+        else:
+            return False
 
 if __name__ == '__main__':
-    task_run = ProcessHtml()
+    task_run = CompileJsonFiles()
     task_run.handle()
