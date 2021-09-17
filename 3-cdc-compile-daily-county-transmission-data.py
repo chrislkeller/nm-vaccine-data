fc8f22a57e64bdbf388f4590faec0643cadb2ed6
Chris Keller <christopherlawrencekeller@gmail.com>
2021-09-16 21:33:56 -0600

    fixes this shiz

--- 3-cdc-compile-daily-county-transmission-data.py
+++ 3-cdc-compile-daily-county-transmission-data.py
@@ -6,6 +6,7 @@ import datetime
 import os
 from os import listdir
 from os.path import isfile, join
+from shutil import copyfile
 import logging
 import pandas as pd
 
@@ -30,17 +31,18 @@ class BuildLatestData(object):
     def handle(self):
         latest_csv = "latest-daily-cdc-county-transmission.csv"
         latest_json = "latest-daily-cdc-county-transmission.json"
-        latest_output = []
         files = [os.path.join(self.path, f) for f in listdir(
             self.path) if isfile(join(self.path, f))]
         target = max(files, key=os.path.getctime)
-        with open(target, encoding='utf-8') as f:
-            raw_json = json.load(f)
-            for item in raw_json['integrated_county_latest_external_data']:
-                item['acquired_datestamp'] = os.path.basename(target)[:10]
-                latest_output.append(item)
-        self.update_csv(latest_csv, latest_output)
-        self.create_json(latest_csv, latest_json, latest_output)
+        file_saved = os.path.join(self.dir_current, self.dir_data, latest_json)
+        copyfile(target, file_saved)
+        # with open(target, encoding='utf-8') as f:
+        #     raw_json = json.load(f)
+        #     for item in raw_json['integrated_county_latest_external_data']:
+        #         item['acquired_datestamp'] = os.path.basename(target)[:10]
+        # latest_output.append(item)
+        # self.update_csv(latest_csv, latest_output)
+        # self.create_json(latest_csv, latest_json, latest_output)
 
     def update_csv(self, file, data):
         file_saved = os.path.join(self.dir_current, self.dir_data, file)
